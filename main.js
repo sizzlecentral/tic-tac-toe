9f5523bf2534aea2f6087509db604dca3e1470b2
Don Cooper <don_cooper@hotmail.com>
2017-07-11 13:40:48 -0400

    Board set up with squares, each have unique id

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  board = document.querySelector('.board');
-  board.style.width = '50%';
-  board.style.height = '50%';
-  board.style.top = '50%';
-  board.style.left = '50%';
-  board.style.backgroundColor = 'red';
-
 });
+
+// x = 88
+// o = 79
