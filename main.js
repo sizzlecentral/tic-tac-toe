18d7775ff76234529e2f956aac97a96ec5bf557d
Don Cooper <don_cooper@hotmail.com>
2017-07-11 16:23:35 -0400

    Click event code refactored, duplicate clicks on a square no longer allowed

--- main.js
+++ main.js
@@ -2,120 +2,53 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var clickCount = 0;
 
+  var board = document.querySelector('.board');
+
+  var clickEvent = function(event) {
+    if (this.innerHTML === '') {
+      clickCount++;
+      var img = document.createElement('img');
+      if (clickCount % 2 === 1) {
+        img.src = 'X.png';
+        this.classList.add('player1');
+      } else {
+        img.src = 'O.png';
+        this.classList.add('player2');
+      }
+      this.appendChild(img);
+    }
+  };
+
   var newGame = document.querySelector('.refresh');
   newGame.addEventListener('click', function() {
     location.reload();
   });
 
   var square1 = document.querySelector('#square1');
-  square1.addEventListener('click', function(event) {
-    clickCount++;
-    var img = document.createElement('img');
-    if (clickCount % 2 === 1) {
-      img.src = 'X.png';
-    } else {
-      img.src = 'O.png';
-    }
-    square1.append(img);
-  });
+  square1.addEventListener('click', clickEvent);
 
   var square2 = document.querySelector('#square2');
-  square2.addEventListener('click', function(event) {
-    clickCount++;
-    var img = document.createElement('img');
-    if (clickCount % 2 === 1) {
-      img.src = 'X.png';
-    } else {
-      img.src = 'O.png';
-    }
-    square2.append(img);
-  });
+  square2.addEventListener('click', clickEvent);
 
   var square3 = document.querySelector('#square3');
-  square3.addEventListener('click', function(event) {
-    clickCount++;
-    var img = document.createElement('img');
-    if (clickCount % 2 === 1) {
-      img.src = 'X.png';
-    } else {
-      img.src = 'O.png';
-    }
-    square3.append(img);
-  });
+  square3.addEventListener('click', clickEvent);
 
   var square4 = document.querySelector('#square4');
-  square4.addEventListener('click', function(event) {
-    clickCount++;
-    var img = document.createElement('img');
-    if (clickCount % 2 === 1) {
-      img.src = 'X.png';
-    } else {
-      img.src = 'O.png';
-    }
-    square4.append(img);
-  });
+  square4.addEventListener('click', clickEvent);
 
   var square5 = document.querySelector('#square5');
-  square5.addEventListener('click', function(event) {
-    clickCount++;
-    var img = document.createElement('img');
-    if (clickCount % 2 === 1) {
-      img.src = 'X.png';
-    } else {
-      img.src = 'O.png';
-    }
-    square5.append(img);
-  });
+  square5.addEventListener('click', clickEvent);
 
   var square6 = document.querySelector('#square6');
-  square6.addEventListener('click', function(event) {
-    clickCount++;
-    var img = document.createElement('img');
-    if (clickCount % 2 === 1) {
-      img.src = 'X.png';
-    } else {
-      img.src = 'O.png';
-    }
-    square6.append(img);
-  });
+  square6.addEventListener('click', clickEvent);
 
   var square7 = document.querySelector('#square7');
-  square7.addEventListener('click', function(event) {
-    clickCount++;
-    var img = document.createElement('img');
-    if (clickCount % 2 === 1) {
-      img.src = 'X.png';
-    } else {
-      img.src = 'O.png';
-    }
-    square7.append(img);
-  });
+  square7.addEventListener('click', clickEvent);
 
   var square8 = document.querySelector('#square8');
-  square8.addEventListener('click', function(event) {
-    clickCount++;
-    var img = document.createElement('img');
-    if (clickCount % 2 === 1) {
-      img.src = 'X.png';
-    } else {
-      img.src = 'O.png';
-    }
-    square8.append(img);
-  });
+  square8.addEventListener('click', clickEvent);
 
   var square9 = document.querySelector('#square9');
-  square9.addEventListener('click', function(event) {
-    clickCount++;
-    var img = document.createElement('img');
-    if (clickCount % 2 === 1) {
-      img.src = 'X.png';
-    } else {
-      img.src = 'O.png';
-    }
-    square9.append(img);
-  });
+  square9.addEventListener('click', clickEvent);
 
 }); // End of document listener
-
-// x = 88
-// o = 79
