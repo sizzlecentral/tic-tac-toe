dacc30b296adb65d017a3e77ccc44140d4899129
Don Cooper <don_cooper@hotmail.com>
2017-07-11 10:20:35 -0400

    Divs set up for board and squares on board

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  board = document.querySelector('.board');
+  board.style.width = '50%';
+  board.style.height = '50%';
+  board.style.top = '50%';
+  board.style.left = '50%';
+  board.style.backgroundColor = 'red';
+
+});
